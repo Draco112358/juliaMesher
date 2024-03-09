9696eb4e88767380f8757589d659a61bdccc7ea4
Draco112358 <gianni.frittella@gmail.com>
2024-03-09 13:48:42 +0100

    new repo for julia mesher

---
+++ lib/mesher.jl
@@ -0,0 +1,235 @@
+include("voxelizator.jl")
+include("voxelize_internal.jl")
+using MeshIO
+using FileIO
+using Meshes
+using MeshBridge
+
+function find_mins_maxs(mesh_object::Mesh)
+    bb = boundingbox(mesh_object)
+    #@assert mesh_object isa Mesh
+    minx = coordinates(minimum(bb))[1]
+    maxx = coordinates(maximum(bb))[1]
+    miny = coordinates(minimum(bb))[2]
+    maxy = coordinates(maximum(bb))[2]
+    minz = coordinates(minimum(bb))[3]
+    maxz = coordinates(maximum(bb))[3]
+    return minx, maxx, miny, maxy, minz, maxz
+end
+
+
+function find_box_dimensions(dict_meshes::Dict)
+    global_min_x, global_min_y, global_min_z = prevfloat(typemax(Float64)), prevfloat(typemax(Float64)), prevfloat(typemax(Float64))
+    global_max_x, global_max_y, global_max_z = -prevfloat(typemax(Float64)), -prevfloat(typemax(Float64)), -prevfloat(typemax(Float64))
+
+    for (key,value) in dict_meshes
+        #println(dict_meshes)
+        minx, maxx, miny, maxy, minz, maxz = find_mins_maxs(value)
+        global_min_x = min(global_min_x, minx)
+        global_min_y = min(global_min_y, miny)
+        global_min_z = min(global_min_z, minz)
+        global_max_x = max(global_max_x, maxx)
+        global_max_y = max(global_max_y, maxy)
+        global_max_z = max(global_max_z, maxz)
+    end
+
+    keeper_object = Dict()
+    keeper_object["meshXmin"] = global_min_x
+    keeper_object["meshXmax"] = global_max_x
+    keeper_object["meshYmin"] = global_min_y
+    keeper_object["meshYmax"] = global_max_y
+    keeper_object["meshZmin"] = global_min_z
+    keeper_object["meshZmax"] = global_max_z
+
+    w = keeper_object["meshXmax"] - keeper_object["meshXmin"]
+    l = keeper_object["meshYmax"] - keeper_object["meshYmin"]
+    h = keeper_object["meshZmax"] - keeper_object["meshZmin"]
+
+    return w, l, h, keeper_object
+end
+
+
+function find_sizes(number_of_cells_x::Int, number_of_cells_y::Int, number_of_cells_z::Int, geometry_descriptor::Dict)
+    
+    @assert number_of_cells_x isa Int
+    @assert number_of_cells_y isa Int
+    @assert number_of_cells_z isa Int
+    @assert geometry_descriptor isa Dict
+    @assert length(geometry_descriptor)==6
+
+    # minimum_vertex_coordinates = [geometry_descriptor['meshXmin'] * 1e-3, geometry_descriptor['meshYmin'] * 1e-3,
+    #           geometry_descriptor['meshZmin'] * 1e-3]
+    # # max_v = [minmax.meshXmax minmax.meshYmax minmax.meshZmax]*1e-3;
+    xv = LinRange(geometry_descriptor["meshXmin"] * 1e-3, geometry_descriptor["meshXmax"] * 1e-3,
+                     number_of_cells_x + 1)
+    yv = LinRange(geometry_descriptor["meshYmin"] * 1e-3, geometry_descriptor["meshYmax"] * 1e-3,
+                     number_of_cells_y + 1)
+    zv = LinRange(geometry_descriptor["meshZmin"] * 1e-3, geometry_descriptor["meshZmax"] * 1e-3,
+                     number_of_cells_z + 1)
+
+    return abs(xv[3] - xv[2]), abs(yv[3] - yv[2]), abs(zv[3] - zv[2])#, minimum_vertex_coordinates
+end
+
+function dump_json_data(filename,n_materials,o_x::Float64,o_y::Float64,o_z::Float64,cs_x::Float64,cs_y::Float64,cs_z::Float64,nc_x,nc_y,nc_z,matr,id_to_material)
+    
+    #print("Serialization to:",filename)
+    @assert cs_x isa Float64
+    @assert cs_y isa Float64
+    @assert cs_z isa Float64
+    @assert o_x isa Float64
+    @assert o_y isa Float64
+    @assert o_z isa Float64
+
+    origin = Dict("origin_x" => o_x, "origin_y" => o_y, "origin_z" => o_z)
+    
+    n_cells = Dict("n_cells_x" => convert(Float64, nc_x),"n_cells_y"=> convert(Float64, nc_y),"n_cells_z"=> convert(Float64, nc_z))
+
+    cell_size = Dict("cell_size_x" => cs_x,"cell_size_y" => cs_y,"cell_size_z" => cs_z)
+
+    materials = Dict()
+    for element in id_to_material
+        materials[element.first]=id_to_material[element.first]
+    end
+        
+    mesher_matrices_dict = Dict()
+
+    
+    for c in range(1, n_materials)
+        x = []
+        for i in range(1, nc_x)
+            push!(x, slicematrix(matr[c,i,:,:]))
+        end
+        mesher_matrices_dict[id_to_material[c]] = x
+        
+        # for matrix in eachslice(matr2, dims=1)
+        #     #@assert count in keys(id_to_material)
+        #     println("->")
+        #     display(matrix)
+        #     
+        #     #display(mesher_matrices_dict[id_to_material[count]])
+        #     #count += 1
+        # end
+    end
+
+    
+    #@assert count == n_materials+1
+    json_dict = Dict("n_materials" => n_materials,"materials" => materials, "origin" => origin , "cell_size" => cell_size, "n_cells" => n_cells, "mesher_matrices" => mesher_matrices_dict)
+    return json_dict
+end
+
+        
+function doMeshing(dictData::Dict)
+
+    meshes = Dict()
+    for geometry in Array{Any}(dictData["STLList"])
+        #@assert geometry isa Dict
+        mesh_id = geometry["material"]
+        mesh_stl = geometry["STL"]
+        #@assert mesh_id not in meshes
+        open("/tmp/stl.stl", "w") do write_file
+            write(write_file,mesh_stl)
+        end
+        mesh_stl = load("/tmp/stl.stl")
+        mesh_stl_converted = convert(Meshes.Mesh, mesh_stl)
+    
+        #mesh_stl_converted = Meshes.Polytope(3,3,mesh_stl)
+        #@assert mesh_stl_converted isa Mesh
+        meshes[mesh_id] = mesh_stl_converted
+        Base.Filesystem.rm("/tmp/stl.stl", force=true)
+    end
+    
+    geometry_x_bound, geometry_y_bound, geometry_z_bound, geometry_data_object = find_box_dimensions(meshes)
+    
+
+    # grids grainx
+    # assert type(dictData['quantum'])==list
+    quantum_x, quantum_y, quantum_z = dictData["quantum"]
+
+    if (geometry_x_bound < quantum_x)
+        return Dict("x" => "too large", "max_x" => geometry_x_bound)
+    elseif (geometry_y_bound < quantum_y)
+        return Dict("y" => "too large", "max_y" => geometry_y_bound)
+    elseif (geometry_z_bound < quantum_z)
+        return Dict("z" => "too large", "max_z" => geometry_z_bound)
+    else
+        # quantum_x, quantum_y, quantum_z = 1, 1e-2, 1e-2 #per Test 1
+        # # quantum_x, quantum_y, quantum_z = 1e-1, 1, 1e-2  # per Test 2
+        # # quantum_x, quantum_y, quantum_z = 1e-1, 1e-1, 1e-2  # per Test 3
+        # # quantum_x, quantum_y, quantum_z = 2, 1, 1e-2  # per Test 4
+        # # quantum_x, quantum_y, quantum_z = 1, 1, 1e-2  # per Test 5
+
+        #print("QUANTA:",quantum_x, quantum_y, quantum_z)
+
+        n_of_cells_x = ceil(Int, geometry_x_bound / quantum_x)
+        n_of_cells_y = ceil(Int, geometry_y_bound / quantum_y)
+        n_of_cells_z = ceil(Int, geometry_z_bound / quantum_z)
+        
+
+        
+        #print("GRID:",n_of_cells_x, n_of_cells_y, n_of_cells_z)
+        
+        cell_size_x, cell_size_y, cell_size_z = find_sizes(n_of_cells_x, n_of_cells_y, n_of_cells_z, geometry_data_object)
+        
+        #precision = 0.1
+        #print("CELL SIZE AFTER ADJUSTEMENTS:",(cell_size_x), (cell_size_y), (cell_size_z))
+        # if __debug__:
+            
+        #     for size,quantum in zip([cell_size_x,cell_size_y,cell_size_z],[quantum_x,quantum_y,quantum_z]):
+        #         print(abs(size*(1/precision) - quantum),precision)
+        #         assert abs(size*(1/precision) - quantum)<=precision
+                
+
+        
+        n_materials = length(dictData["STLList"])
+        
+        mesher_output = fill(false, (n_materials,n_of_cells_x, n_of_cells_y, n_of_cells_z))
+
+        mapping_ids_to_materials = Dict()
+
+        counter_stl_files = 1
+        for mesh_id in meshes
+            #@assert meshes[mesh_id] isa Mesh
+            mesher_output[counter_stl_files,:,:,:] = voxelize(n_of_cells_x, n_of_cells_y, n_of_cells_z, meshes[mesh_id.first], geometry_data_object)
+
+            mapping_ids_to_materials[counter_stl_files]=mesh_id.first
+            counter_stl_files+=1
+        end
+
+        id_mats_keep=zeros(Int64, n_materials)
+        
+        id_mats_keep[1]=0
+        
+        solve_overlapping(n_of_cells_x, n_of_cells_y, n_of_cells_z, n_materials, id_mats_keep, mesher_output)
+            
+
+
+
+        origin_x = geometry_data_object["meshXmin"]*1e-3
+        origin_y = geometry_data_object["meshYmin"]*1e-3
+        origin_z = geometry_data_object["meshZmin"]*1e-3
+
+
+        # assert(isinstance(mesher_output, np.ndarray))
+        # @assert cell_size_x isa Float64
+        # @assert cell_size_y isa Float64
+        # @assert cell_size_z isa Float64
+        # @assert origin_x isa Float64
+        # @assert origin_y isa Float64
+        # @assert origin_z isa Float64
+
+        # Writing to data.json
+        json_file = "outputMesher.json"
+        
+        return dump_json_data(json_file, counter_stl_files-1, origin_x, origin_y, origin_z, cell_size_x, cell_size_y, cell_size_z,
+                n_of_cells_x, n_of_cells_y, n_of_cells_z, mesher_output, mapping_ids_to_materials) 
+    end
+end
+
+function slicematrix(A::AbstractMatrix{T}) where T
+    m, n = size(A)
+    B = Vector{T}[Vector{T}(undef, n) for _ in 1:m]
+    for i in 1:m
+        B[i] .= A[i, :]
+    end
+    return B
+end
---
+++ lib/voxelizator.jl
@@ -0,0 +1,257 @@
+using Meshes
+
+function solve_overlapping(n_cells_x,n_cells_y,n_cells_z,num_materials,id_mat_keep,output_meshing)
+
+    for c1 in range(1, n_cells_x)
+        for c2 in range(1, n_cells_y)
+            for c3 in range(1, n_cells_z)
+                for k in range(1, num_materials)
+                    if output_meshing[k,c1, c2, c3] == true
+                        for k2 in range(1, num_materials)
+                            if output_meshing[k2,c1, c2, c3] == true && k!=k2
+                                if k in id_mat_keep
+                                    output_meshing[k2, c1, c2, c3] = false
+                                else
+                                    output_meshing[k, c1, c2, c3] = false
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+function merge_the_3_grids(voxcountX, voxcountY, voxcountZ,gridOUTPUT1,gridOUTPUT2,gridOUTPUT3,gridOUTPUT)
+    for c1 in range(1, voxcountX)
+        for c2 in range(1, voxcountY)
+            for c3 in range(1,voxcountZ)
+                cont_true = 0
+                if gridOUTPUT1[c2, c3, c1] == true
+                    cont_true = cont_true + 1
+                end
+                if gridOUTPUT2[c3, c1, c2] == true
+                    cont_true = cont_true + 1
+                end
+                if gridOUTPUT3[c1, c2, c3] == true
+                    cont_true = cont_true + 1
+                end
+                if cont_true > 1
+                    gridOUTPUT[c1, c2, c3] = true
+                end
+            end
+        end
+    end
+    return gridOUTPUT
+end
+
+function voxelize(cells_on_x::Int,cells_on_y::Int,cells_on_z::Int,meshXYZ::Mesh, geometry_desc::Dict)
+
+    #@assert cells_on_x isa Int
+    #@assert cells_on_y isa Int
+    #@assert cells_on_ isa Int
+    #@assert geometry_desc isa Dict
+
+    #@assert length(geometry_desc)==6
+    
+    # raydirection    = 'xyz'
+    
+    #@assert meshXYZ isa Mesh
+       
+    meshXmin = geometry_desc["meshXmin"]
+    meshXmax = geometry_desc["meshXmax"]
+    meshYmin = geometry_desc["meshYmin"]
+    meshYmax = geometry_desc["meshYmax"]
+    meshZmin = geometry_desc["meshZmin"]
+    meshZmax = geometry_desc["meshZmax"]
+
+    if cells_on_x==1
+        #If gridX is a single integer (rather than a vector) and is equal to 1
+        gridCOx   = (meshXmin+meshXmax)/2
+    else
+        #If gridX is a single integer (rather than a vector) then automatically create the list of x coordinates
+        voxwidth  = (meshXmax-meshXmin)/(cells_on_x+1/2)
+        gridCOx = [meshXmin+voxwidth/2]
+        for (index, value) in enumerate(range(meshXmin+voxwidth/2, meshXmax-voxwidth/2, step=voxwidth))
+            if (index != 1)
+                push!(gridCOx, gridCOx[index-1]+voxwidth)
+            end
+        end
+    end
+    
+    if cells_on_y==1
+        #If gridX is a single integer (rather than a vector) and is equal to 1
+        gridCOy   = (meshYmin+meshYmax)/2
+    else
+        #If gridX is a single integer (rather than a vector) then automatically create the list of x coordinates
+        voxwidth  = (meshYmax-meshYmin)/(cells_on_y+1/2)
+        gridCOy = [meshYmin+voxwidth/2]
+        for (index, value) in enumerate(range(meshYmin+voxwidth/2, meshYmax-voxwidth/2, step=voxwidth))
+            if (index != 1)
+                push!(gridCOy, gridCOy[index-1]+voxwidth)
+            end
+        end
+    end
+    
+    if cells_on_z==1
+        #If gridX is a single integer (rather than a vector) and is equal to 1
+        gridCOz   = (meshZmin+meshZmax)/2
+    else
+        #If gridX is a single integer (rather than a vector) then automatically create the list of x coordinates
+        voxwidth  = (meshZmax-meshZmin)/(cells_on_z+1/2)
+        gridCOz = [meshZmin+voxwidth/2]
+        for (index, value) in enumerate(range(meshZmin+voxwidth/2, meshZmax-voxwidth/2, step=voxwidth))
+            if (index != 1)
+                push!(gridCOz, gridCOz[index-1]+voxwidth)
+            end
+        end
+    end
+
+    #@assert minimum(gridCOx)>meshXmin
+    # Check that the output grid is large enough to cover the mesh:
+    var_check=0
+    if (minimum(gridCOx)>meshXmin || maximum(gridCOx)<meshXmax)
+        var_check = 1
+        gridcheckX = 0
+        if minimum(gridCOx)>meshXmin
+            gridCOx=vcat(meshXmin, gridCOx)
+            gridcheckX += 1
+        end
+        if maximum(gridCOx)<meshXmax
+            gridCOx = vcat(gridCOx,meshXmax)
+            gridcheckX += 2
+        end
+    else
+        if (minimum(gridCOy)>meshYmin || maximum(gridCOy)<meshYmax)
+            var_check = 2
+            gridcheckY = 0
+            if minimum(gridCOy)>meshYmin
+                gridCOy = vcat(meshYmin, gridCOy)
+                gridcheckY += 1
+            end
+            if maximum(gridCOy)<meshYmax
+                gridCOy = vcat(gridCOy, meshYmax)
+                gridcheckY += 2
+            end
+        else
+            if (minimum(gridCOz)>meshZmin || maximum(gridCOz)<meshZmax)
+                var_check = 3
+                gridcheckZ = 0
+                if minimum(gridCOz)>meshZmin
+                    gridCOz = vcat(meshZmin, gridCOz)
+                    gridcheckZ += 1
+                end
+                if maximum(gridCOz)<meshZmax
+                    gridCOz = vcat(gridCOz, meshZmax)
+                    gridcheckZ += 2
+                end
+            end
+        end
+    end
+    
+
+    # %======================================================
+    # % VOXELISE USING THE USER DEFINED RAY DIRECTION(S)
+    # %======================================================
+    
+    # Count the number of voxels in each direction:
+    voxcountX = length(gridCOx)
+    voxcountY = length(gridCOy)
+    voxcountZ = length(gridCOz)
+
+
+    v0 = zeros(convert(Int64, length(vertices(meshXYZ))/3), 3)
+    v1 = zeros(convert(Int64, length(vertices(meshXYZ))/3), 3)
+    v2 = zeros(convert(Int64, length(vertices(meshXYZ))/3), 3)
+
+    i = 1
+    j = 1
+    k = 1
+
+    #println(vertices(meshXYZ))
+
+    for (index, p) in enumerate(vertices(meshXYZ))
+        if (mod(index, 3) == 1)
+            v0[i,1] = coordinates(p)[1]
+            v0[i,2] = coordinates(p)[2]
+            v0[i,3] = coordinates(p)[3]
+            i = i + 1
+        end
+        if (mod(index, 3) == 2)
+            v1[j,1] = coordinates(p)[1]
+            v1[j,2] = coordinates(p)[2]
+            v1[j,3] = coordinates(p)[3]
+            j = j + 1
+        end
+        if (mod(index, 3) == 0)
+            v2[k,1] = coordinates(p)[1]
+            v2[k,2] = coordinates(p)[2]
+            v2[k,3] = coordinates(p)[3]
+            k = k + 1
+        end
+        #println(coordinates(p))
+    end
+
+    # display(v0)
+    # display(v1)
+    # display(v2)
+
+    gridOUTPUT1 = voxel_intern(gridCOy, gridCOz, gridCOx, v0, v1, v2, geometry_desc, 0)
+    gridOUTPUT2 = voxel_intern(gridCOz, gridCOx, gridCOy, v0, v1, v2, geometry_desc, 1)
+    gridOUTPUT3 = voxel_intern(gridCOx, gridCOy, gridCOz, v0, v1, v2, geometry_desc, 2)
+
+    #from scipy.io import savemat
+    #mdic = {"gridOUTPUT1": gridOUTPUT1, "gridOUTPUT2": gridOUTPUT2, "gridOUTPUT3": gridOUTPUT3}
+    #savemat("python_grids.mat", mdic)
+
+    gridOUTPUT = fill(false, (voxcountX, voxcountY, voxcountZ))
+    gridOUTPUT = merge_the_3_grids(voxcountX, voxcountY, voxcountZ, gridOUTPUT1, gridOUTPUT2, gridOUTPUT3,gridOUTPUT)
+
+    # %======================================================
+    # % RETURN THE OUTPUT GRID TO THE SIZE REQUIRED BY THE USER (IF IT WAS CHANGED EARLIER)
+    # %======================================================
+    #@assert var_check in [1,2,3]
+    # match var_check:
+    if var_check == 1
+        #@assert gridcheckX in [1,2,3]
+        if gridcheckX == 1
+            gridOUTPUT=gridOUTPUT[2:voxcountX,:,:]
+            gridCOx    = gridCOx[2:voxcountX]
+        elseif gridcheckX == 2
+            gridOUTPUT = gridOUTPUT[1:voxcountX-1, :, :]
+            gridCOx = gridCOx[1:voxcountX-1]
+        else
+            gridOUTPUT = gridOUTPUT[2:voxcountX-1, :, :]
+            gridCOx = gridCOx[2:voxcountX-1]
+        end
+    elseif var_check == 2
+        #@assert gridcheckY in [1,2,3]
+            if gridcheckY == 1
+                gridOUTPUT = gridOUTPUT[:,2:voxcountY, :]
+                gridCOy = gridCOy[2:voxcountY]
+            elseif gridcheckY == 2
+                gridOUTPUT = gridOUTPUT[:, 1:voxcountY-1, :]
+                gridCOy = gridCOy[1:voxcountY-1]
+            else
+                gridOUTPUT = gridOUTPUT[:, 2:voxcountY-1, :]
+                gridCOy = gridCOy[2:voxcountY-1]
+            end
+    else
+        #@assert var_check == 3
+        #@assert gridcheckZ in [1,2,3]
+        if gridcheckZ == 1
+            gridOUTPUT = gridOUTPUT[:, :, 2:voxcountZ]
+            gridCOz = gridCOz[2:voxcountZ]
+        elseif gridcheckZ == 2
+            gridOUTPUT = gridOUTPUT[:, :, 1:voxcountZ-1]
+            gridCOz = gridCOz[1:voxcountZ-1]
+        else
+            gridOUTPUT = gridOUTPUT[:, :, 2:voxcountZ-1]
+            gridCOz = gridCOz[2:voxcountZ-1]
+        end
+    end
+
+    #if needed also gridCOx, gridCOy and gridCOz can be given in output
+    return gridOUTPUT
+end

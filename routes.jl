9fef1421209ca62308951e22afaa54e226caf4e8
Draco112358 <gianni.frittella@gmail.com>
2024-04-28 16:18:31 +0200

    aggiunta funzione per precompilazione delle rotte in fase di avvio.

--- routes.jl
+++ routes.jl
@@ -6,18 +6,30 @@ Genie.config.run_as_server = true
 Genie.config.cors_headers["Access-Control-Allow-Origin"] = "http://localhost:1212"
 # This has to be this way - you should not include ".../*"
 Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
-Genie.config.cors_headers["Access-Control-Allow-Methods"] ="GET,POST,PUT,DELETE,OPTIONS" 
+Genie.config.cors_headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
 Genie.config.cors_allowed_origins = ["*"]
 
-route("/") do
-  serve_static_file("welcome.html")
+# route("/") do
+#   serve_static_file("welcome.html")
+# end
+
+
+route("/meshing", method="POST") do
+  return JSON.json(doMeshing(jsonpayload()))
 end
 
-
-route("/meshing" ,method="POST") do 
-    return JSON.json(doMeshing(jsonpayload()))
-end
-
-route("/meshingAdvice" ,method="POST") do 
+route("/meshingAdvice", method="POST") do
   return JSON.json(quantumAdvice(jsonpayload()))
 end
+
+function force_compile()
+  sleep(5)
+  println("------ Precompiling routes...wait for mesher to be ready ---------")
+  for (name, r) in Router.named_routes()
+    data = open(JSON.parse, "first_run_data.json")
+    Genie.Requests.HTTP.request(r.method, "http://localhost:8003" * tolink(name), [("Content-Type", "application/json")], JSON.json(data))
+  end
+  println("------------- MESHER READY ---------------")
+end
+
+@async force_compile()
